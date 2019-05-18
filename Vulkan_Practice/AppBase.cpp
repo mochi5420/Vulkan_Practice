c2dd3de264bf7c90b860aa797944bbdb7781f036
mochi5420 <k.mochida5420@gmail.com>
2019-05-19 02:06:04 +0900

    Instanceを生成する関数を追加

---
+++ Vulkan_Practice/AppBase.cpp
@@ -0,0 +1,68 @@
+#include "AppBase.h"
+
+// Createなどの結果を受け判定をおこなう
+void AppBase::CheckResult(VkResult result)
+{
+	if (result != VK_SUCCESS)
+	{
+		DebugBreak();
+	}
+}
+
+
+// Instanceを生成する
+void AppBase::InitializeInstance(const char* appName)
+{
+	// ApplicationInfoの初期化
+	VkApplicationInfo appInfo;
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pNext = nullptr;
+	appInfo.pApplicationName = appName;
+	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.pEngineName = appName;
+	appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.apiVersion = VK_API_VERSION_1_1;
+
+	// extensionの取得
+	std::vector<const char*> extensions;
+	std::vector<VkExtensionProperties> props;
+	{
+		uint32_t count = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &count, nullptr);
+		props.resize(count);
+		vkEnumerateInstanceExtensionProperties(nullptr, &count, props.data());
+
+		for (const auto& v : props)
+		{
+			extensions.push_back(v.extensionName);
+		}
+	}
+
+	// CreateInfoの初期化
+	VkInstanceCreateInfo ci;
+	ci.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	ci.pNext = nullptr;
+	ci.flags = 0;
+	ci.pApplicationInfo = &appInfo;
+
+#ifdef _DEBUG
+	// Debug時は下記のmeta layerを有効にする
+	const char* layers[] = { "VK_LAYER_LUNARG_standard_validation" };
+	ci.enabledLayerCount = 1;
+	ci.ppEnabledLayerNames = layers;
+#endif
+
+	ci.enabledExtensionCount = uint32_t(extensions.size());
+	ci.ppEnabledExtensionNames = extensions.data();
+
+	// Instanceの生成
+	auto result = vkCreateInstance(&ci, nullptr, &_instance);
+	CheckResult(result);
+}
+
+
+// 物理デバイスを取得する
+void AppBase::GetPhysicalDevice()
+{
+	
+}
---
+++ Vulkan_Practice/AppBase.h
@@ -0,0 +1,30 @@
+#pragma once
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#define VK_USE_PLATFORM_WIN32_KHR
+#define GLFW_INCLUDE_VULKAN
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
+#include <vulkan/vk_layer.h>
+#include <vulkan/vulkan_win32.h>
+
+#pragma comment(lib, "vulkan-1.lib")
+
+#include <vector>
+
+class AppBase
+{
+public:
+
+private:
+	void InitializeInstance(const char* appName);
+	void GetPhysicalDevice();
+
+	void CheckResult(VkResult result);
+
+	VkInstance _instance;
+
+};
+
